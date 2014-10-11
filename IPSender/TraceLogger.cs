71c68da8b4ef43dd43eef832fc7751f323eecd06
Olli <olli@nic.fi>
2014-10-11 19:23:13 +0300

    Adding TraceLogger

---
+++ IPSender/TraceLogger.cs
@@ -0,0 +1,28 @@
+﻿namespace IPSender
+{
+    using System;
+    using System.Configuration;
+    using System.Diagnostics;
+
+    public static class TraceLogger
+    {   
+        public static void Write(string message)
+        {
+            bool loggingEnabled;
+
+            if (!bool.TryParse(ConfigurationManager.AppSettings.Get("LoggingEnabled"), out loggingEnabled))
+            {
+                Trace.Write(DateTime.Now.ToString("s") + " : " + "LoggingEnabled parameter not found in application configuration file");
+                Trace.Write(DateTime.Now.ToString("s") + " : " + message);
+                Trace.Write(Environment.NewLine);
+            }
+            
+            if(loggingEnabled)
+            {
+                Trace.Write(DateTime.Now.ToString("s") + " : " + message);
+                Trace.Write(Environment.NewLine);
+            }
+            
+        }
+    }
+}
